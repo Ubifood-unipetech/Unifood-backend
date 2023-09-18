fe0d47f5d3ee709c8e5dfd13c5958bf5a2ccf84e
Kosolov325 <xxlucas325xx@gmail.com>
2023-09-18 00:36:05 -0300

    Authentication endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework import routers                  
 from api.restaurants import Restaurants
 from api.products import Products, ProductTypes
-from api.authentication import Login
+from api.authentication import Login, Register, Logout
 
 router = routers.DefaultRouter()      
 router.register('restaurants',Restaurants)         
@@ -11,7 +11,8 @@ router.register('product/types', ProductTypes)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', Login.as_view(), name='token_obtain_pair')
-
+    path('login/', Login.as_view(), name='token_obtain_pair'),
+    path('register/', Register.as_view(), name='register_view'),
+    path('logout/', Logout.as_view(), name='logout'),
 ]
 
